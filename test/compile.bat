a02bb6883d4d6a7155020afdd326f52d3728de8a
745826631@qq.com <48887241+ysyisyourbrother@users.noreply.github.com>
2019-05-28 20:10:01 +0800

    Update compile.bat

--- test/compile.bat
+++ test/compile.bat
@@ -1,5 +1,5 @@
 @echo off
-title  a.cpp
+title  testing...
 pushd %~d0
-g++ -std=c++11 a.cpp&&a
+g++ -std=c++11 E:\OneDrive\Coding\My-Leetcode\test\test.cpp -o E:\OneDrive\Coding\My-Leetcode\test\test&&E:\OneDrive\Coding\My-Leetcode\test\test
 pause
