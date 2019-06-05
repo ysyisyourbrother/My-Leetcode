e1a0557954d824677d1281883af33da80fde3c95
745826631@qq.com <48887241+ysyisyourbrother@users.noreply.github.com>
2019-06-05 17:36:37 +0800

    del

--- test/compile.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title  testing...
-pushd %~d0
-g++ -std=c++11 E:\OneDrive\Coding\My-Leetcode\test\test.cpp -o E:\OneDrive\Coding\My-Leetcode\test\test&&E:\OneDrive\Coding\My-Leetcode\test\test
-pause
